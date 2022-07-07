c4ef354503127ac33de5c46bc530f53d213d89df
Samandar <uchqunovsamandar@mail.ru>
2022-07-07 01:36:32 -0700

    bug fixed and other user experienses

--- client-app/src/app/contact/contact.component.ts
+++ client-app/src/app/contact/contact.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Animations } from '../animations/animations';
+import emailjs, { EmailJSResponseStatus } from '@emailjs/browser';
 
 @Component({
   selector: 'app-contact',
@@ -9,8 +10,30 @@ import { Animations } from '../animations/animations';
 export class ContactComponent implements OnInit {
 
   constructor() { }
-
+  name:string = "";
   ngOnInit(): void {
+  }
+  sendEmail(e: Event,nameVal: HTMLInputElement) {
+    //https://dashboard.emailjs.com/admin/settings/profile
+    //email.js
+    e.preventDefault();
+    if(nameVal.value)
+      emailjs
+        .sendForm(
+          'service_x2fyqdr',
+          'template_jmymgdm',
+          e.target as HTMLFormElement,
+          'OTZ7LndlPExRYxV2w'
+        )
+        .then(
+          (result: EmailJSResponseStatus) => {
+            console.log(result.text);
+            alert("xabar jo'natildi")
+          },
+          (error) => {
+            console.log(error.text);
+          }
+        );
   }
 
 }
